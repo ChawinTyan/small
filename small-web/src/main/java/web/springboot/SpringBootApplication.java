ca15be8c88d5c38a852a988be19dfc7971993163
chawintyan <chawintyan@aliyun.com>
2016-06-06 13:45:05 +0800

    spring security init

--- small-web/src/main/java/web/springboot/SpringBootApplication.java
+++ small-web/src/main/java/web/springboot/SpringBootApplication.java
@@ -1,12 +1,19 @@
 package web.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by tianwenyuan on 2016/6/6.
  */
 @org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootApplication {
+@ComponentScan({"web.controller.security"})
+public class SpringBootApplication{
     public static void main(String args[]) {
         SpringApplication.run(SpringBootApplication.class, args);
     }
